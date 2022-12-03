8ba574e3443bee878dc7bd058cdc6233ea86a980
moodyfox124 <moodyfox124@gmail.com>
2022-12-03 13:33:32 +0300

    solution

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,22 @@ use anyhow::{Result, Ok};
 fn main() -> Result<()> {
     let input = fs::read_to_string("./input.txt")?;
     let mut floor = 0;
+    let mut basement_enter_character_position: Option<usize> = None;
+    let mut is_basement_character_found = false;
 
-    input.chars().for_each(|character| {
+    input.chars().enumerate().for_each(|(index, character)| {
         match character {
             '(' => floor +=1,
             ')' => floor -=1,
-            _ => return, 
+            _ => return,
         };
+        if is_basement_character_found == false && floor == -1 {
+            basement_enter_character_position = Some(index + 1);
+            is_basement_character_found = true;
+        }
     });
 
     println!("Santa will end up on floor {}", floor);
+    println!("Santa went in the basement after instruction in position {}", basement_enter_character_position.unwrap());
     return Ok(());
 }
