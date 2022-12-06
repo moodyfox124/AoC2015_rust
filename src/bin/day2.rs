89a21a79a5f6f7e7cedfae9920c51c73da994364
moodyfox124 <moodyfox124@gmail.com>
2022-12-06 04:20:45 +0300

    utileze some rust features

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,17 +1,46 @@
+use anyhow;
 use anyhow::Result;
-use std::fs;
+use std::{fs, str::FromStr};
 
-fn calculate_amount_of_present_paper(length: usize, width: usize, height: usize) -> Result<usize> {
+#[derive(Debug)]
+struct PresentSizes {
+    values: [usize; 3],
+}
+
+impl FromStr for PresentSizes {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let sizes: Vec<usize> = s
+            .split('x')
+            .map(|v| {
+                v.parse::<usize>()
+                    .expect("provided string contain non number value")
+            })
+            .collect();
+        Ok(Self {
+            values: [sizes[0], sizes[1], sizes[2]],
+        })
+    }
+}
+
+fn calculate_amount_of_present_paper(sizes: &PresentSizes) -> usize {
+    let PresentSizes {
+        values: [length, width, height],
+    } = sizes;
     if let Some(v) = [length * width, width * height, height * length]
         .iter()
         .min()
     {
-        return Ok((2 * length * width + 2 * width * height + 2 * height * length) + v);
+        return (2 * length * width + 2 * width * height + 2 * height * length) + v;
     };
-    return Ok(0usize);
+    return 0;
 }
 
-fn calculate_ribbon(length: usize, width: usize, height: usize) -> usize {
+fn calculate_ribbon(sizes: &PresentSizes) -> usize {
+    let &PresentSizes {
+        values: [length, width, height],
+    } = sizes;
     let bow = length * width * height;
     let max = [length, width, height].iter().max().unwrap().to_owned();
     let min_sides: Vec<usize> = [length, width, height]
@@ -23,22 +52,21 @@ fn calculate_ribbon(length: usize, width: usize, height: usize) -> usize {
 }
 
 fn main() -> Result<()> {
-    let mut total_paper = 0;
-    let mut total_ribbon = 0;
-    let input = fs::read_to_string("./day2_input.txt")?;
-    input.lines().for_each(|line| {
-        let sizes: Vec<usize> = line
-            .split('x')
-            .map(|v| v.parse::<usize>().unwrap())
-            .collect();
-        let paper = calculate_amount_of_present_paper(sizes[0], sizes[1], sizes[2]).unwrap();
-        total_paper += paper;
+    let required_paper = fs::read_to_string("./day2_input.txt")?
+        .lines()
+        .map(|v| PresentSizes::from_str(v))
+        .flatten()
+        .fold((0, 0), |init, v| {
+            (
+                init.0 + calculate_amount_of_present_paper(&v),
+                init.1 + calculate_ribbon(&v),
+            )
+        });
 
-        let ribbon = calculate_ribbon(sizes[0], sizes[1], sizes[2]);
-        total_ribbon += ribbon;
-    });
-
-    println!("total square feet of wrapping paper is {}", total_paper);
-    println!("total square feet of ribbon is {}", total_ribbon);
+    println!(
+        "total square feet of wrapping paper is {}",
+        required_paper.0
+    );
+    println!("total square feet of ribbon is {}", required_paper.1);
     Ok(())
 }
