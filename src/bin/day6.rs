57724639e010381d0ea937ae6ef577494c696b4f
moodyfox124 <moodyfox124@gmail.com>
2023-01-03 09:16:24 +0300

    Part1 solution

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -3,9 +3,9 @@ use std::str::FromStr;
 
 #[derive(Debug)]
 enum Operation {
-    toogle,
-    on,
-    off,
+    TOOGLE,
+    ON,
+    OFF,
 }
 
 #[derive(Debug)]
@@ -25,11 +25,11 @@ impl FromStr for Instruction {
         let instruction_and_start: Vec<&str> = raw_instructions[0].trim().split(' ').collect();
 
         let (operation, start_position): (Operation, Vec2) = match instruction_and_start.len() {
-            2 => (Operation::toogle, instruction_and_start[1].parse()?),
+            2 => (Operation::TOOGLE, instruction_and_start[1].parse()?),
             3 => {
                 let operation = match instruction_and_start[1].trim() {
-                    "on" => Operation::on,
-                    "off" => Operation::off,
+                    "on" => Operation::ON,
+                    "off" => Operation::OFF,
                     _ => return Err(anyhow::Error::msg("unexpected operation")),
                 };
                 (operation, instruction_and_start[2].parse()?)
@@ -45,14 +45,14 @@ impl FromStr for Instruction {
     }
 }
 
-#[derive(Debug)]
-struct Vec2(i32, i32);
+#[derive(Debug, Clone, Copy)]
+struct Vec2(usize, usize);
 
 impl FromStr for Vec2 {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut points: Vec<i32> = Vec::new();
+        let mut points: Vec<usize> = Vec::new();
         for point in s.trim().split(',') {
             let value = point.parse()?;
             points.push(value);
@@ -61,14 +61,63 @@ impl FromStr for Vec2 {
     }
 }
 
+#[derive(Debug)]
 struct Grid {
-    on: Vec<Vec2>,
+    lights: Vec<[bool; 1000]>,
 }
 
+impl Grid {
+    fn new() -> Self {
+        Self {
+            lights: vec![[false; 1000]; 1000],
+        }
+    }
+
+    fn operate(&mut self, instruction: &Instruction) {
+        match instruction.operation {
+            Operation::ON => {
+                for i in instruction.start.0..=instruction.end.0 {
+                    for j in instruction.start.1..=instruction.end.1 {
+                        self.lights[i][j] = true;
+                    }
+                }
+            }
+            Operation::OFF => {
+                for i in instruction.start.0..=instruction.end.0 {
+                    for j in instruction.start.1..=instruction.end.1 {
+                        self.lights[i][j] = false;
+                    }
+                }
+            }
+            Operation::TOOGLE => {
+                for i in instruction.start.0..=instruction.end.0 {
+                    for j in instruction.start.1..=instruction.end.1 {
+                        self.lights[i][j] = !self.lights[i][j];
+                    }
+                }
+            }
+        };
+    }
+}
 
 fn main() {
     let input = std::fs::read_to_string("./day6_input.txt").expect("input file should present");
     let instructions: Vec<Instruction> = input.lines().map(|line| line.parse()).flatten().collect();
-    println!("{:#?}", instructions);
 
+    let mut grid = Grid::new();
+    instructions
+        .iter()
+        .for_each(|instruction| grid.operate(instruction));
+
+    let mut on_lights = 0;
+
+    for i in 0..1000 {
+        for j in 0..1000 {
+            if grid.lights[i][j] {
+                on_lights += 1;
+            }
+        }
+    }
+
+    println!("turned on lights: {}", on_lights);
 }
